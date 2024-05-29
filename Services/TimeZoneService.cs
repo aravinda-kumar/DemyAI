abc73831f439d4297e61a73be8acef8548a337ad
egomez <egomezr@outlook.com>
2024-05-29 12:17:07 +0200

    Create timezones

---
+++ Services/TimeZoneService.cs
@@ -0,0 +1,14 @@
+﻿namespace DemyAI.Services;
+public class TimeZoneService {
+
+    static List<string> GetTimeZones() {
+
+        return [
+            "Europe/Madrid",
+            "America/Guayaquil",
+            "Europe/Madrid",
+            "America/New_York",
+            "Asia/Riyadh",
+        ];
+    }
+}
